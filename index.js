278d73aed8dc33f70d5c969822d77886e5c7f2d1
Nivetha s <akshaya2d@gmail.com>
2022-03-05 21:47:50 +0530

    Update index.js
    Update require to import

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import path from "path";
 
 dotenv.config();
 
@@ -21,7 +22,6 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("memories-client-main/build"));
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "memories-client-main", "build", "index.html")
