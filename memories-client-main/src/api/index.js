945be99c9417cc63ab88f8e2cfd4e79749439639
Nivetha s <akshaya2d@gmail.com>
2022-03-05 21:49:01 +0530

    Update index.js

--- memories-client-main/src/api/index.js
+++ memories-client-main/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://joyce-memories-app-server.herokuapp.com/posts';
+const url = '/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
