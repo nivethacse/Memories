0cdc804e9fee7580431772eb70b82dc6320e20c0
Nivetha s <akshaya2d@gmail.com>
2022-03-10 23:26:12 +0530

    Update App.js

--- memories-client-main/src/App.js
+++ memories-client-main/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">SLAM BOOK</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">slam book</Typography>
         <!--<img className={classes.image} src={memories} alt="icon" height="60" />-->
       </AppBar>
       <Grow in>
