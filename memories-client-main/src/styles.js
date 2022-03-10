fb3b4e91ab49dffaac95648905709b3a7116f0c2
Nivetha s <akshaya2d@gmail.com>
2022-03-10 23:47:55 +0530

    Update styles.js

--- memories-client-main/src/styles.js
+++ memories-client-main/src/styles.js
@@ -10,7 +10,7 @@ export default makeStyles(() => ({
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'rgba(0,0,0, 0.5)',
   },
   image: {
     marginLeft: '15px',
